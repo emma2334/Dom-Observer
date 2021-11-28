7b773f43b094b2ed07d288aba6768cd0d5f24fa3
Emma <emma2334@gmail.com>
2021-11-29 02:42:31 +0800

    Add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import style from './App.module.css'
+import demo from './demo'
 
 const App = () => {
   return (
     <div className={style.App}>
       <div className={style.container}>
-        <textarea id={style.html} />
+        <div id={style.html} contentEditable={true}>
+          {demo}
+        </div>
         <button id={style.convert}>Convert</button>
-        <div id={style.dom} />
+        <div id={style.dom} dangerouslySetInnerHTML={{ __html: demo }} />
       </div>
     </div>
   )
