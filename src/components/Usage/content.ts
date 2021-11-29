648413241f2fbce17fdcec49f8ba3f856bdb1884
Emma <emma2334@gmail.com>
2021-11-30 07:47:38 +0800

    Add description

---
+++ src/components/Usage/content.ts
@@ -0,0 +1,6 @@
+export default [
+  'Click "convert" to preview the code in editor ',
+  'Code would be synced to editor when DOM changes.',
+  'Script tags would be removed.',
+  'Styles in style tags would be convert to inline styles',
+]
---
+++ src/components/Usage/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import content from './content'
+
+import style from './index.module.css'
+
+const Editor = () => {
+  return (
+    <div id={style.usage}>
+      <h3>Usage</h3>
+      <ul>
+        {content.map(e => (
+          <li>{e}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default React.memo(Editor)
